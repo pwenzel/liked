9d293ba66cf2f46e215dc21db90b35c78f39cfbb
Paul Wenzel <pwenzel@mpr.org>
2014-05-01 15:22:18 -0500

    CreateEntriesTable migration

---
+++ app/database/migrations/2014_05_01_195540_create_entries_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('entries', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->timestamps();
+			$table->dateTime('date_saved')->nullable();
+			$table->string('url');
+			$table->mediumText('title')->nullable();
+			$table->text('description')->nullable();
+			$table->text('content')->nullable();
+			$table->dateTime('article_date')->nullable();
+			$table->text('keywords')->nullable();
+			$table->text('entities')->nullable();
+			$table->text('image')->nullable();
+			$table->text('favicon')->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('entries');
+	}
+
+}
