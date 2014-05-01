220c927078c3e353791ca36718342fe1346d2bcc
Paul Wenzel <pwenzel@mpr.org>
2014-05-01 16:14:15 -0500

    additional server vars in sample

--- env.local.sample.php
+++ env.local.sample.php
@@ -1,11 +1,19 @@
 <?php
 
+/**
+ * Copy this file to .env.local.php
+ * Be sure to include the '.' prefix to make it a hidden file
+ * http://laravel.com/docs/configuration#protecting-sensitive-configuration
+ */
+
 return array(
 
-    'PANDORA_USERNAME'	=> '',
-    'EMBEDLY_API_KEY'	=> '',
-    'DATABASE'			=> '',
-    'DATABASE_USERNAME' => '',
-	'DATABASE_PASSWORD' => '',
+    'PANDORA_USERNAME'		=> '',
+    'LASTFM_USERNAME'		=> '',
+    'INSTAPAPER_USERNAME'	=> '',
+    'EMBEDLY_API_KEY'		=> '',
+    'DATABASE'				=> '',
+    'DATABASE_USERNAME'		=> '',
+	'DATABASE_PASSWORD'		=> '',
 
 );
